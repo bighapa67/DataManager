145463f160d5038ca7847398159ed56aceacf48d
Ken <kjones67@gmail.com>
2019-10-26 01:19:26 -0500

    Completed the code for OHLC requests to the API.

--- PolygonData.py
+++ PolygonData.py
@@ -1,72 +1,38 @@
 import json
 import requests
 import os
+import datetime as dt
 
 os.environ['API_KEY'] = 'Xq_bQM92tq78l3FNagTWix06raWaq7y1ptr7_t'
-url = 'https://api.polygon.io/v1/last/stocks/'
+histRangeUrl = 'https://api.polygon.io/v2/aggs/ticker/'
 symbol = 'AAPL'
+startDate = '2019-01-01'
+endDate = '2019-02-01'
+unadjusted = 'false'
 
-jsonResponse = requests.get(url + symbol + '?apiKey=' + os.environ['API_KEY'])
-pythonObject = jsonResponse.json()
+queryString = (histRangeUrl + symbol + '/range/1/day/' + startDate + '/' + endDate + '?unadjusted=' \
+                            + unadjusted + '&apiKey=' + os.environ['API_KEY'])
 
-#Source: https://bcmullins.github.io/parsing-json-python/
-def extract_element_from_json(obj, path):
-    '''
-    Extracts an element from a nested dictionary or
-    a list of nested dictionaries along a specified path.
-    If the input is a dictionary, a list is returned.
-    If the input is a list of dictionary, a list of lists is returned.
-    obj - list or dict - input dictionary or list of dictionaries
-    path - list - list of strings that form the path to the desired element
-    '''
-    def extract(obj, path, ind, arr):
-        '''
-            Extracts an element from a nested dictionary
-            along a specified path and returns a list.
-            obj - dict - input dictionary
-            path - list - list of strings that form the JSON path
-            ind - int - starting index
-            arr - list - output list
-        '''
-        key = path[ind]
-        if ind + 1 < len(path):
-            if isinstance(obj, dict):
-                if key in obj.keys():
-                    extract(obj.get(key), path, ind + 1, arr)
-                else:
-                    arr.append(None)
-            elif isinstance(obj, list):
-                if not obj:
-                    arr.append(None)
-                else:
-                    for item in obj:
-                        extract(item, path, ind, arr)
-            else:
-                arr.append(None)
-        if ind + 1 == len(path):
-            if isinstance(obj, list):
-                if not obj:
-                    arr.append(None)
-                else:
-                    for item in obj:
-                        arr.append(item.get(key, None))
-            elif isinstance(obj, dict):
-                arr.append(obj.get(key, None))
-            else:
-                arr.append(None)
-        return arr
-    if isinstance(obj, dict):
-        return extract(obj, path, 0, [])
-    elif isinstance(obj, list):
-        outer_arr = []
-        for item in obj:
-            outer_arr.append(extract(item, path, 0, []))
-        return outer_arr
+jsonResponse = requests.get(queryString)
+responseDict = jsonResponse.json()
 
-#lastPx = pythonObject['price']
-lastPx = extract_element_from_json(pythonObject, ['last', 'price'])
+#Nice simple explanation of how to handle nested dictionary JSON responses:
+#https://stackoverflow.com/questions/51788550/parsing-json-nested-dictionary-using-python
+ticker = responseDict['ticker']
+resultsDict = responseDict['results']
+for x in resultsDict:
+    openPx = x['o']
+    highPx = x['h']
+    lowPx = x['l']
+    closePx = x['c']
+    volume = x['v']
+    rawDate = x['t']
+    convDate = dt.datetime.fromtimestamp(rawDate / 1000).strftime('%Y-%m-%d')
 
-print(pythonObject)
-print(lastPx)
-# print(response.status_code)     # To print http response code
-# print(response.text)            # To print formatted JSON response
+    print('Ticker: ' + str(ticker))
+    print('Open: ' + str(openPx))
+    print('High: ' + str(highPx))
+    print('Low: ' + str(lowPx))
+    print('Close: ' + str(closePx))
+    print('Volume: ' + str(volume))
+    print('Date: ' + str(convDate))
