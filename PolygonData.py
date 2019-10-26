c8803205a5c64a478f635ecc4546df109af027d9
Ken <kjones67@gmail.com>
2019-10-25 21:55:13 -0500

    Rec'd my first response!

---
+++ PolygonData.py
@@ -0,0 +1,72 @@
+import json
+import requests
+import os
+
+os.environ['API_KEY'] = 'Xq_bQM92tq78l3FNagTWix06raWaq7y1ptr7_t'
+url = 'https://api.polygon.io/v1/last/stocks/'
+symbol = 'AAPL'
+
+jsonResponse = requests.get(url + symbol + '?apiKey=' + os.environ['API_KEY'])
+pythonObject = jsonResponse.json()
+
+#Source: https://bcmullins.github.io/parsing-json-python/
+def extract_element_from_json(obj, path):
+    '''
+    Extracts an element from a nested dictionary or
+    a list of nested dictionaries along a specified path.
+    If the input is a dictionary, a list is returned.
+    If the input is a list of dictionary, a list of lists is returned.
+    obj - list or dict - input dictionary or list of dictionaries
+    path - list - list of strings that form the path to the desired element
+    '''
+    def extract(obj, path, ind, arr):
+        '''
+            Extracts an element from a nested dictionary
+            along a specified path and returns a list.
+            obj - dict - input dictionary
+            path - list - list of strings that form the JSON path
+            ind - int - starting index
+            arr - list - output list
+        '''
+        key = path[ind]
+        if ind + 1 < len(path):
+            if isinstance(obj, dict):
+                if key in obj.keys():
+                    extract(obj.get(key), path, ind + 1, arr)
+                else:
+                    arr.append(None)
+            elif isinstance(obj, list):
+                if not obj:
+                    arr.append(None)
+                else:
+                    for item in obj:
+                        extract(item, path, ind, arr)
+            else:
+                arr.append(None)
+        if ind + 1 == len(path):
+            if isinstance(obj, list):
+                if not obj:
+                    arr.append(None)
+                else:
+                    for item in obj:
+                        arr.append(item.get(key, None))
+            elif isinstance(obj, dict):
+                arr.append(obj.get(key, None))
+            else:
+                arr.append(None)
+        return arr
+    if isinstance(obj, dict):
+        return extract(obj, path, 0, [])
+    elif isinstance(obj, list):
+        outer_arr = []
+        for item in obj:
+            outer_arr.append(extract(item, path, 0, []))
+        return outer_arr
+
+#lastPx = pythonObject['price']
+lastPx = extract_element_from_json(pythonObject, ['last', 'price'])
+
+print(pythonObject)
+print(lastPx)
+# print(response.status_code)     # To print http response code
+# print(response.text)            # To print formatted JSON response
