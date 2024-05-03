db61891e56dbc7d315fcffabe3ed3157a9cae043
bighapa67 <kjones67@gmail.com>
2024-05-03 10:08:38 -0500

    Created a session check to determine the date to use for validation.

--- DataConductor.py
+++ DataConductor.py
@@ -6,6 +6,7 @@ from database_connector import DatabaseConnector
 from sqlalchemy import Table, select, join
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 """
 This program is meant to be the conductor that orchestrates the capture and recording of my CEF related data.
@@ -103,13 +104,56 @@ def process_columns(df):
     df['TimeUpdated'] = df['TimeUpdated'].dt.date
     return df
 
+def date_check(df):
+    """
+    Check if the price date and NAV date are the same. 
+    I think I'm going to pick three funds and compare THOSE dates.
+    If the dates don't match then I need to figure out how to handle that.
+    1) I could use a filter to create a df of only the matching dates that also match the expected date.
+       Those records would then go through bulk insertion with the offenders being logged and reported.
+    2) Alert me of the issue and I'll manually handle it.
+    """
+  
+    df['date_matches'] = df['TimeUpdated'] == df['Date']
+    print(df['date_matches'].value_counts())
+    print(df[df['date_matches'] == False])
+
+    return df
+
+def determine_session():
+    """
+    This function will determine the session to use based on the time of day.
+    It will then use that information to determine the date to use for validation.
+    The RealTick NAV data grab is sometimes done at 7pm CT (session "T") and sometimes the next morning
+    (session "T+1"), resulting in discrepancies in the dates in the tables when compared to "today".
+    """
+    today = datetime.today()
+    if today.hour < 19:
+        print("Running the ""Date T-1"" session (i.e. grabbing yesterday's final NAV data).")
+        validation_date = today.date() - timedelta(days=1)
+        print(validation_date)
+    else:
+        print("Running the ""Date T"" session. We are expecting the NAV data to have been updated for today.")
+        validation_date = today.date()
+
+    return validation_date
+
 
 if __name__ == "__main__":
+    validation_date = determine_session()
     symbol_df = fetch_data(HistoricalPriceDB_conn, "CEF_Setup", ["Symbol", "MFQSSym"])
     price_df = fetch_data(RealTick_LiveData_conn, "RealTimeData", ["Symbol", "TimeUpdated", "[Open]", "DayHigh", "DayLow", "CurrentPrice"])
     nav_df = fetch_data(HistoricalPriceDB_conn, "RealtickNAVs", ["Symbol", "Date", "ClosePx"])
     merged_df = merge_dataframes(symbol_df, price_df, nav_df)
     processed_df = process_columns(merged_df)
+    # This is a good time to do a check on the price date (coming from RealTimeData) and the NAV date (coming from RealtickNAVs).
+    date_check_df = date_check(processed_df)
+
+    # Make two separate dfs for the records that match and those that don't.
+    # The matching records will be bulk inserted into the CEF_price_nav_history table.
+    # I do need to run a check on the history table and/or have an alert sent if there's an issue.
+    # I need to account for the variability of when this script runs.
+
     print(processed_df.dtypes)
     print()
     print(processed_df)
